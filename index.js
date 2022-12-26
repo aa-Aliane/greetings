4ab5f688bd2739f3fa2bd98cc3ca03812a3b6f71
aaa29 <aliane781@gmail.com>
2022-12-26 13:27:39 +0100

    fc

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const API_URL = "api/v1/";
+const app = express();
+
+app.get(API_URL + "greetings", (req, res) => {
+  res.status(200).send("hello");
+});
+
+app.listen(5000);
